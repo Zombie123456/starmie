88c8606d01570f0be5b388269801df51e7045c16
2938129812@qq.com <chalice@unnotech.com>
2019-07-04 17:25:24 +0800

    f

--- src/urls.js
+++ src/urls.js
@@ -1,7 +1,7 @@
 'use strict'
 const config = require('../config')
 const env = process.env.NODE_ENV === 'development' ? config.dev.env : config.build.env
-const host = 'http://api.chalice.top/backend_api'
+const host = 'https://api.60929lp.com'
 const prefix = host + '/v1/member/'
 export default {
     announcements: prefix + 'announcement/',
